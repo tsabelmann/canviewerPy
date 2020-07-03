43336ca56a92ec40b71b3231f9cc1d06ded170d8
Tim Lucas Sabelmann <tsa@ecap-mobility.com>
2020-07-03 15:40:41 +0200

    feat: Changed main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 import argparse
-from gtkmm.MainWindow import MainWindow
+from canviewer.gui.MainWindow import MainWindow
 
 window = MainWindow()
 window.connect("destroy", Gtk.main_quit)
